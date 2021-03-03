3617102239578a458787ab6f972cfc8992063d1d
Leone_Beta <leoneftd.if@gmail.com>
2021-03-03 18:53:37 -0300

    att

--- infodir.c
+++ infodir.c
@@ -1,3 +1,7 @@
+/*o programa infodir pode criar até 4 funções, sendo elas, 3 para os 3 primeiros diretórios, e o quarto 
+verificando todos os diretorios e arquivos restantes, caso so tenha arquivos na pasta pesquisada, criar um 
+processo filho para calcular o tamanho dos arquivos*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,7 +15,8 @@
 //Memoria compartilhada
 typedef struct{
     char dirName[20];
-}TDir;
+    int sizeDir;
+}TInfo;
 
 void removeEnter(char string[]){
 	int tamanho = strlen(string) - 1;
@@ -25,17 +30,15 @@ void removeEnter(char string[]){
 
 //Função que a thread irá executar
 void* funcThread(void *firstPtr){
-    TDir *ptr;
-    ptr = (TDir*) firstPtr;     //Cast do ponteiro do tipo void para ponteiro do tipo TDir
-
-    
+    TInfo *ptr;
+    ptr = (TInfo*) firstPtr;     //Cast do ponteiro do tipo void para ponteiro do tipo TDir
 }
 
 int start(){
     pthread_t thread1_ID,thread2_ID,thread3_ID,thread4_ID;                  //ID da Thread
     void *thread1_return,*thread2_return,*thread3_return,*thread4_return;   //Ponteiro de retorno
     int thread1,thread2,thread3,thread4;                                    //variavel de validação da Thread
-    TDir strDir, *ptrDir;       //struct e ponteiro para a struct de memoria compartilhada
+    TInfo strDir, *ptrDir;       //struct e ponteiro para a struct de memoria compartilhada
     ptrDir = &strDir;           //passando o endereço da struct para o ponteiro
 
     //Variaveis de Diretório,
@@ -50,10 +53,11 @@ int start(){
 
     if((dir = opendir(caminhoDir)) != NULL){
         while((ent = readdir(dir)) != NULL){
-            printf("\n%s: ",ent->d_name);
+            printf("%d\n ",ent->d_reclen);
             
-
-            //Verificar a quantidade de diretorios existentes
+/*
+            //Verificar a quantidade de diretorios existentes e criar 3 threads para percorrer esses diretórios
+            //e uma thread para armazenando e somanod o tamanho dos diretórios pesquisados
 
 
              //Função pthread_create passando cmo parametros o endereço do ID da thread, NULL, função que a thread irá
@@ -64,7 +68,7 @@ int start(){
                 printf("\nErro ao iniciar a Thread");
                 exit(EXIT_FAILURE);
             }
-
+*/
         }
     }else{
         perror("");
